9225f5e5a5ede91b3e55d511329c9d2e467ae8f0
Naseeh AN <naseehan700@gmail.com>
2024-03-29 10:34:05 +0530

    email already in use

--- server.js
+++ server.js
@@ -68,6 +68,13 @@ const Cart = mongoose.model("Cart", cartSchema);
 app.post("/signup", async (req, res) => {
   try {
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    // if email already exists
+    if(existingUser){
+      return res.status(400).json({ message: "Email already in use"})
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ email, password: hashedPassword });
     await user.save();
